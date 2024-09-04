d05c776c7094cd31fe0968a6efc0d7d8804d2095
Hasib <hasib.jhd@gmail.com>
2024-09-04 20:17:13 +0600

    scrollview slider with page control

--- Scanner/PageViewController.swift
+++ Scanner/PageViewController.swift
@@ -6,13 +6,23 @@
 //
 
 import UIKit
-
+//https://tobyliu-sw.medium.com/uiscrollview-part-2-paging-enabled-with-uipagecontrol-937619a0c57d
 class PageViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pageControlValueChanged(_ sender: UIPageControl) {
+        print(#function)
+//        print(sender.)
+        let pageSize: CGSize = self.scrollView.bounds.size
+        let pageOrigin: CGPoint = CGPoint(x: CGFloat(sender.currentPage) * pageSize.width, y: 0.0)
+        
+        // Scroll to corresponding page when current page index of page control is changed
+        self.scrollView.scrollRectToVisible(CGRect(origin: pageOrigin, size: pageSize), animated: true)
+    }
 }
